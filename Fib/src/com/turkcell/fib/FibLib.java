e1b425c5561e2e995aadef72f42f04f0ab6a8bbb
Marko Gargenta <marko@marakana.com>
2012-02-28 04:44:18 -0800

    Implemented FibLib and fib module

---
+++ Fib/src/com/turkcell/fib/FibLib.java
@@ -0,0 +1,16 @@
+package com.turkcell.fib;
+
+public class FibLib {
+
+	public static long fibJ(long n) {
+		if(n==0) return 0;
+		if(n==1) return 1;
+		return fibJ(n-1)+fibJ(n-2);
+	}
+	
+	public static native long fibN(long n);
+	
+	static {
+		System.loadLibrary("fib");
+	}
+}
