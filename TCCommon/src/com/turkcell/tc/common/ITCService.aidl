77ded45ea08c83b5f6711853047bf7de7eb9b439
Marko Gargenta <marko@marakana.com>
2012-02-29 00:54:08 -0800

    Changed executeCommand() to return String

--- TCCommon/src/com/turkcell/tc/common/ITCService.aidl
+++ TCCommon/src/com/turkcell/tc/common/ITCService.aidl
@@ -1,5 +1,5 @@
 package com.turkcell.tc.common;
 
 interface ITCService {
-	void executeCommand(String command);
+	String executeCommand(String command);
 }
--- TCService/src/com/turkcell/tc/service/ITCServiceImpl.java
+++ TCService/src/com/turkcell/tc/service/ITCServiceImpl.java
@@ -1,6 +1,8 @@
 package com.turkcell.tc.service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import android.os.RemoteException;
 import android.util.Log;
@@ -9,13 +11,22 @@ import com.turkcell.tc.common.ITCService;
 
 public class ITCServiceImpl extends ITCService.Stub {
 	static final String TAG = "ITCServiceImpl";
-	
-	public void executeCommand(String command) throws RemoteException {
+
+	public String executeCommand(String command) throws RemoteException {
 		try {
-			Runtime.getRuntime().exec(command);
+			StringBuffer ret = new StringBuffer();
+			Process p = Runtime.getRuntime().exec(command);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				ret.append(line);
+			}
+			return ret.toString();
 		} catch (IOException e) {
-			Log.e(TAG, "Failed to execute command: " + command, e);
+			String message = "Failed to execute command: " + command;
+			Log.e(TAG, message, e);
+			return message;
 		}
 	}
-
 }
